334bd8dfd6e1c12eef558d44479fe6dd184646bc
Christopher P.K Morris <christopher.pkm@gmail.com>
2016-02-06 19:41:01 +0000

    breaking it down to get 1 array split

--- MergeSort.java
+++ MergeSort.java
@@ -26,28 +26,4 @@ public class MergeSort {
 
       System.out.println("ran");
     }
-
-
-    private static void divide(int oldLeft[], int oldRight[]){
-      left = new int[oldLeft.length / 2];
-      right = new int[oldRight.length / 2];
-
-      //for (int i = 0; i < arr.length; i++) {
-
-        // copy second half of an array and put into a new one.
-
-        System.arraycopy(arr, 0, left, 0, arr1.length / 2);
-        System.arraycopy(arr2, arr2.length / 2, right, 0, arr2.length / 2);
-
-      //}
-      System.out.println("left: " + Arrays.toString(left));
-      System.out.println("right: " + Arrays.toString(right));
-
-      if(left.length == 1) operation = false;
-
-      divide(left, right); // recursion
-
-    }
 }
-
-// copy, duplicate, check if divisible by 2, check if greater than 1
