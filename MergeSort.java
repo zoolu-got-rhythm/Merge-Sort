5c5a5f31a2bb223f880cfd3757971b6993aec09b
Christopher P.K Morris <christopher.pkm@gmail.com>
2016-02-06 23:43:11 +0000

    split working

--- MergeSort.java
+++ MergeSort.java
@@ -4,7 +4,7 @@ public class MergeSort {
 
     Boolean operation = true; // divide = true, add = false
 
-    public static Boolean main(String args[]) {
+    public static void main(String args[]) {
       int[] myIntArray = {5, 4, 2, 1};
 
       sort(myIntArray);
@@ -12,8 +12,8 @@ public class MergeSort {
 
     private static void sort(int arr[]){
       // inital split on parsed array
-      int left = new int[arr.length / 2]; // set up memory space
-      int right = new int[arr.length / 2]; // set up memory space
+      int[] left = new int[arr.length / 2]; // set up memory space
+      int[] right = new int[arr.length / 2]; // set up memory space
 
       System.arraycopy(arr, 0, left, 0, arr.length / 2);
       System.arraycopy(arr, arr.length / 2, right, 0, arr.length / 2);
