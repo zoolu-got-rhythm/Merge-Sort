bdc5f58b9d55a19a742ab1018adc2d536e9d5d57
Christopher P.K Morris <christopher.pkm@gmail.com>
2016-02-07 00:30:43 +0000

    working on recursive divide method

--- MergeSort.java
+++ MergeSort.java
@@ -2,15 +2,15 @@ import java.util.Arrays;
 
 public class MergeSort {
 
-    Boolean operation = true; // divide = true, add = false
+    public static Boolean operation = true; // divide = true, add = false
 
     public static void main(String args[]) {
       int[] myIntArray = {5, 4, 2, 1, 8, 3, 7, 9};
-
+      
       sort(myIntArray);
     }
 
-    private static void sort(int arr[]){
+    public static void sort(int arr[]){
       // inital split on parsed array
       int[] left = new int[arr.length / 2]; // set up memory space
       int[] right = new int[arr.length / 2]; // set up memory space
@@ -19,17 +19,35 @@ public class MergeSort {
       System.arraycopy(arr, arr.length / 2, right, 0, arr.length / 2);
 
       // if(operation){
-      //   divide(left, right);
+         divide(left, right);
       // }
       //else{
         // merge(); // recursion
       //}
 
-      for (int i = 0; i < left.length; i++ ) {
-        System.out.println("left: " + left[i]);
-        System.out.println("right: " + right[i]);
+      System.out.println("left: " + Arrays.toString(left));
+      System.out.println("right: " + Arrays.toString(right));
+
+    }
+
+    public static void divide(int oldLeft[], int oldRight[]){
+      int[] left = new int[oldLeft.length / 2];
+      int[] right = new int[oldRight.length / 2];
+
+        System.arraycopy(oldLeft, 0, left, 0, oldLeft.length / 2);
+        System.arraycopy(oldRight, oldRight.length / 2, right, 0, oldRight.length / 2);
+
+      System.out.println("left: " + Arrays.toString(left));
+      System.out.println("right: " + Arrays.toString(right));
+
+      if(left.length != 1){
+        divide(left, right); // recursion
+      } else{
+        operation = false;
       }
 
 
+
     }
+
 }
